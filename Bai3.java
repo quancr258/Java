94bad81db3e2e0c9d0cea2deb191329293d5ab63
Phan MInh Quân <quancr258@yahoo.com.vn>
2018-04-09 15:18:42 +0700

    Lab1

---
+++ Bai3.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab1;
+
+import java.util.Scanner;
+/**
+ *
+ * @author Admin
+ */
+public class Bai3 
+{
+    public static void main(String[] args) 
+    {
+        Scanner nhap = new Scanner(System.in);
+        System.out.println("canh: ");
+        double canh = nhap.nextDouble();
+        double thetich = Math.pow(canh, 3);
+        System.out.printf("the tich: %f \n", thetich);
+    }
+}
